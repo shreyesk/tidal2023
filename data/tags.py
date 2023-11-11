19e14ea6790db5c33508460bcb1f38ef9c41a686
shreyesk <sshreyes.kaliyur@gmail.com>
2023-11-11 12:31:35 -0600

    return tag count from a ratemy prof website

--- data/tags.py
+++ data/tags.py
@@ -12,8 +12,32 @@ chrome_options.add_argument('--headless')  # Run Chrome in headless mode (no GUI
 chrome_service = ChromeService(executable_path='chromedriver_mac64/chromedriver')  # Replace with the path to your chromedriver executable
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-def get_professors(dept, number):
-    url = f'https://www.ratemyprofessors.com/search/professors/1003?q=Thomas%20C'
+def get_tags(url):
+    import requests
+    from bs4 import BeautifulSoup
+
+    # Specify the URL of the website you want to scrape
+    # url = f'https://www.ratemyprofessors.com/professor/2482285'
+
+    # Send an HTTP request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    tag_count = dict()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        tags = soup.find_all('span', {'class': 'hHOVKF'})
+        tags = [tag.text for tag in tags]
+        for tag in tags:
+            if tag not in tag_count.keys():
+                tag_count[tag] = 1
+            else:
+                tag_count[tag] += 1
+    return tag_count
+
+def get_website(name):
+    name = name.replace(' ', '%20') # make it so we can put name inside of html query
+    url = f'https://www.ratemyprofessors.com/search/professors/1003?q={name}'
     TEACHER_CARD_CLASS = 'dLJIlx'
 
     # Open the webpage
@@ -24,40 +48,11 @@ def get_professors(dept, number):
         element_present = EC.presence_of_element_located((By.CLASS_NAME, TEACHER_CARD_CLASS))
         WebDriverWait(driver, timeout=3).until(element_present)
 
-        teacher_card = driver.find_elements(By.CLASS_NAME, TEACHER_CARD_CLASS)[0]
-        teacher_website = teacher_card.get_attribute('href')
-        print(teacher_website)
-
-        # CODE IS GOOD UP TO HERE, prints out teacher website from name search
-
-        element_present = EC.presence_of_element_located((By.TAG_NAME, 'tr'))
-        WebDriverWait(table, timeout=3).until(element_present)
-
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-
-        header = rows[0].find_elements(By.TAG_NAME, 'td')
-        header = [header_cell.text for header_cell in header]
-        target_index = header.index(TARGET_COLUMN)
-
-        rows = rows[1:]
-
-        professors = set()
-        for row in rows:
-            row = row.find_elements(By.TAG_NAME, 'td')
-            row = [row_cell.text for row_cell in row]
-            value = row[target_index]
-            value = value.replace('(H)', '').strip() # remove honors designation
-            professors.add(value)
-
-        print(professors)
-        return list(professors)
-
+        card = driver.find_elements(By.CLASS_NAME, TEACHER_CARD_CLASS)[0]
+        website = card.get_attribute('href')
+        return website
     except Exception as e:
         print(f"An error occurred: {e}")
 
-    finally:
-        # Close the browser
-        driver.quit()
-
 if __name__ == '__main__':
-    get_professors('CHEM', 102)
+    get_tags('Thomas s')
