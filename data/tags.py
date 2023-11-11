324ead7f910413437515ea49ecaa71abf292fa75
shreyesk <sshreyes.kaliyur@gmail.com>
2023-11-11 12:02:56 -0600

    work on getting tags for teacher from name search

--- data/tags.py
+++ data/tags.py
@@ -13,19 +13,22 @@ chrome_service = ChromeService(executable_path='chromedriver_mac64/chromedriver'
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 def get_professors(dept, number):
-    url = f'https://anex.us/grades/?dept={dept}&number={number}'
-    TARGET_COLUMN = 'Prof'
-    TABLE_ID = 'dataTable'
+    url = f'https://www.ratemyprofessors.com/search/professors/1003?q=Thomas%20C'
+    TEACHER_CARD_CLASS = 'dLJIlx'
 
     # Open the webpage
     driver.get(url)
 
     try:
         # Wait for a specific element to load (adjust as needed)
-        element_present = EC.presence_of_element_located((By.ID, TABLE_ID))
+        element_present = EC.presence_of_element_located((By.CLASS_NAME, TEACHER_CARD_CLASS))
         WebDriverWait(driver, timeout=3).until(element_present)
 
-        table = driver.find_element(By.ID, TABLE_ID)
+        teacher_card = driver.find_elements(By.CLASS_NAME, TEACHER_CARD_CLASS)[0]
+        teacher_website = teacher_card.get_attribute('href')
+        print(teacher_website)
+
+        # CODE IS GOOD UP TO HERE, prints out teacher website from name search
 
         element_present = EC.presence_of_element_located((By.TAG_NAME, 'tr'))
         WebDriverWait(table, timeout=3).until(element_present)
